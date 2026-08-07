26341015222a5e5253ebd2dcb599c56f87880654
TheGit-Father <oneadityasinha@gmail.com>
2026-08-07 17:39:05 +0530

    Build two-layer neural network with vectorized forward pass

--- model.py
+++ model.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+def relu(x):
+  return np.maximum(0, x)
+
+class NeuralNetwork:
+  def __init__(self):
+    self.Linear1 = Linear(2, 16)
+    self.Linear2 = Linear(16, 2)
+
+
+  def forward(self, x):
+    z1 = self.Linear1.forward(x)
+    a1 = relu(z1)
+    z2 = self.Linear2.forward(a1)
+    return z2
+
 class Linear:
   def __init__(self, in_features, out_features):
     self.W = np.random.randn(in_features, out_features)
