299757e2bed8c7bf18f55abbcb8f497fe4f6616a
TheGit-Father <oneadityasinha@gmail.com>
2026-08-10 16:52:20 +0530

    Add gradient descent parameter updates

--- model.py
+++ model.py
@@ -35,12 +35,22 @@ class NeuralNetwork:
   def backward(self, probabilities, y):
     Y = np.zeros_like(probabilities)
     Y[np.arange(len(y)), y] = 1
-
     dz2 = (probabilities - Y) / len(y)
-    dw2, db2, da1 = self.Linear2.backward(dz2)
+    dW2, db2, da1 = self.Linear2.backward(dz2)
     dz1 = da1 * (self.z1 > 0)
-    dw1, db1, dX = self.Linear1.backward(dz1)
-    return dw1, db1, dw2, db2
+    dW1, db1, dX = self.Linear1.backward(dz1)
+    return dW1, db1, dW2, db2
+
+  def update(self, dW1, db1, dW2, db2, lr):
+    self.Linear1.W -= lr * dW1
+    self.Linear1.b -= lr * db1
+    self.Linear2.W -= lr * dW2
+    self.Linear2.b -= lr * db2
+
+  def predict(self, X):
+    logits = self.forward(X)
+    probabilities = softmax(logits)
+    return np.argmax(probabilities, axis=1)
 
 
 class Linear:
@@ -54,7 +64,7 @@ class Linear:
 
   def backward(self, dz):
     dW = self.input.T @ dz
-    db = np.sum(dz, axis=0, keepdims=True)
+    db = np.sum(dz, axis=0)
     dX = dz @ self.W.T
     return dW, db, dX
 
